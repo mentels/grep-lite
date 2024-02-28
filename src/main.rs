73474d6da9745f0e4e9c029b45e64c9faaccca1a
Szymon Mentel <mentel.szymon@gmail.com>
2024-02-28 18:48:05 +0100

    Make it to read stdin

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,49 @@
 use clap::{Arg, ArgMatches, Command};
 use regex::Regex;
-use std::{fs::File, io::BufReader, io::BufRead};
-
-fn main() {
-   let args = parse_args(); 
-
-   let pattern = args.get_one::<String>("pattern").unwrap();
-   let re = Regex::new(&pattern).unwrap();
-
-   let input = args.get_one::<String>("input").unwrap();
-   let f = File::open(input).unwrap();
-   let reader = BufReader::new(f);
-
-   print_matches(reader, &re);
-}
+use std::{fs::File, io, io::BufRead, io::BufReader};
 
 fn parse_args() -> ArgMatches {
     return Command::new("grep-lie")
-    .author("Me,me@mail.com")
-    .about("searches for pattern in a file")
-    .version("0.1")
-    .arg(
-        Arg::new("pattern")
-            .help("The pattern to search for")
-            .required(true),
-    )
-    .arg(
-        Arg::new("input")
-            .help("The input file to search")
-            .required(true),
-    )
-    .get_matches();
+        .author("Me,me@mail.com")
+        .about("searches for pattern in a file")
+        .version("0.1")
+        .arg(
+            Arg::new("pattern")
+                .help("The pattern to search for")
+                .required(true),
+        )
+        .arg(Arg::new("input").help("The input file to search"))
+        .get_matches();
 }
 
-fn print_matches(reader: BufReader<File>, re: &Regex) {
+fn print_matches<T: BufRead + Sized>(reader: T, re: Regex) {
     for line_ in reader.lines() {
         let line = line_.unwrap();
-        if re.is_match(&line) {
-            println!("{}", line);
+        match re.find(&line) {
+            Some(_) => println!("{}", line),
+            None => (),
+        }
+    }
+}
+
+fn main() {
+    let args = parse_args();
+
+    let pattern = args.get_one::<String>("pattern").unwrap();
+    let re = Regex::new(&pattern).unwrap();
+
+    let input = args.get_one::<String>("input");
+
+    match input {
+        Some(input) => {
+            let f = File::open(input).unwrap();
+            let reader = BufReader::new(f);
+            print_matches(reader, re);
+        }
+        None => {
+            let stdin = io::stdin();
+            let reader = stdin.lock();
+            print_matches(reader, re);
         }
     }
 }
